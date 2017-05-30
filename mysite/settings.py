ef6890deb7cd0d62a67c9e5ec1a11fe34afc95bb
Gili <selowbutsure1@gmail.com>
2017-05-30 15:40:27 -0400

    View and template for detail and list post

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Jakarta'
+TIME_ZONE = None #Asia/Jakarta'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False #rue
 
 
 # Static files (CSS, JavaScript, Images)
